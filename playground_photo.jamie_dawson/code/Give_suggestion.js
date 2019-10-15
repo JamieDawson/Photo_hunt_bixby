033f674b2efda1d48d7d6ac6b154f4cf4e8b1b08
Jamie Dawson <jadawson@e1z2r5p45.42.us.org>
2019-10-14 19:02:42 -0700

    fix tomorrow

--- playground_photo.jamie_dawson/code/Give_suggestion.js
+++ playground_photo.jamie_dawson/code/Give_suggestion.js
@@ -5,9 +5,15 @@ var http = require('http')
 module.exports.function = function give_suggestion () {
   console.log(CONTENT);
 
+  var all = CONTENT;
+
+  for (var i = 0; i < all.lenght; i++)
+  { 
+      console.log("w")
+
+  }
 
 
 
-
-  return stuff;
+  return CONTENT;
 }
