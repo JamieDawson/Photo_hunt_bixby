f7bd6d03d56cbf504a9ea28873bf4a666e99f70c
JamieDawson <jamierobertdawson@gmail.com>
2019-10-24 11:20:52 -0700

    updated return for js

--- playground_photo.jamie_dawson/code/Give_suggestion.js
+++ playground_photo.jamie_dawson/code/Give_suggestion.js
@@ -6,7 +6,7 @@ module.exports.function = function give_suggestion () {
 
 //var suggestion_array = ["abd", "efg", "hij"]
 
-  var test = "did it"
+  var test = ["hello", "world", "test"]
 
-  return test
+  return test[Math.floor(Math.random() * 3)]
 }
