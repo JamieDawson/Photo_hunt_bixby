0e177c449f4db915e56a271b0ba7d14ddbacb6bc
Jamie Dawson <jadawson@e1z2r6p10.42.us.org>
2019-10-27 21:04:09 -0700

    new file

--- playground_photo.jamie_dawson/code/Give_suggestion.js
+++ playground_photo.jamie_dawson/code/Give_suggestion.js
@@ -6,7 +6,19 @@ module.exports.function = function give_suggestion () {
 
 //var suggestion_array = ["abd", "efg", "hij"]
 
-  var test = ["hello", "world", "test"]
+  var test = ["hello", "world", "test"];
 
-  return test[Math.floor(Math.random() * 3)]
+  var img = ["images/tree.png"];
+
+ // return test[Math.floor(Math.random() * 3)]
+
+var all = {   text: test[Math.floor(Math.random() * 3)],
+   random_pic: {
+     url: img[0]
+   }
+   
+   }
+
+ return all 
+
 }
